b7b5fc979c789894399e2a2fd019dd97eee68c8b
SergiyStebelskiy <sergiy.stebelskiy.01@gmail.com>
2020-07-25 11:52:29 +0300

    app

---
+++ build/precache-manifest.cb02fffff605f242c077bc34a618471e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa504d5500bf1ed6bc310b3de588c74b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d247de1376fb000c8879",
+    "url": "/static/css/main.af1280f1.chunk.css"
+  },
+  {
+    "revision": "e66ef7e0723a535e9b5f",
+    "url": "/static/js/2.c7639906.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c7639906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d247de1376fb000c8879",
+    "url": "/static/js/main.e8a4792f.chunk.js"
+  },
+  {
+    "revision": "228441fd791f26663f57",
+    "url": "/static/js/runtime-main.cb52b832.js"
+  }
+]);
